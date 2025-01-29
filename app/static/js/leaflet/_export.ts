5dcd6b77c562a73f6eb71c014e8c953650df2fca
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2025-01-29 16:55:01 +0100

    move to different file

---
+++ app/static/js/leaflet/_export.ts
@@ -0,0 +1,46 @@
+export const swizzleContext = () => {
+    const originalCreateElement = document.createElement
+    const ctxProxy = {
+        get(target: any, prop: string | symbol) {
+            // Return the function wrapped in a proxy to ensure its behavior is maintained
+            if (typeof target[prop] === "function")
+                return (...args: any[]) => {
+                    // console.log("Calling:", prop)
+                    return target[prop](...args)
+                }
+
+            return target[prop]
+        },
+        set(target: any, prop: string | symbol, value: any) {
+            target[prop] = value
+            return true
+        },
+    }
+
+    document.createElement = (tagName: string, ...args: any[]) => {
+        const element = originalCreateElement.call(document, tagName, ...args)
+
+        if (element instanceof HTMLCanvasElement) {
+            console.debug("Captured canvas creation")
+
+            const originalGetContext = element.getContext.bind(element)
+
+            element.getContext = (type, ...args2) => {
+                console.debug("Captured getContext")
+
+                // Call the original getContext method to get the WebGL context
+                const ctx = originalGetContext(type, ...args2)
+
+                // Only proxy the context if it's a WebGL context
+                if (ctx instanceof WebGL2RenderingContext)
+                    // Create a proxy around the WebGL context
+                    return new Proxy(ctx, ctxProxy)
+
+                // If it's not a WebGL context, just return the context as is
+                return ctx
+            }
+        }
+
+        return element
+    }
+}
